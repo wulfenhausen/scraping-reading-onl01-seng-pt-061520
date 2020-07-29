a5a286ad4d348237642fc10def54231a85f44e0b
William Han Findiesen <william.findiesen@gmail.com>
2020-07-29 18:12:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,4 +4,8 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 doc = Nokogiri::HTML(html)
 
-doc.css(".headline-260IBN").text
+doc.css(".headline-260IBN").text.strip
+
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b .inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
+
+courses.each { |course| puts course.text.strip }
